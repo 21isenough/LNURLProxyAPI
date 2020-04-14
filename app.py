a77ac7f81fd20321886c4157574c6e5a79463c17
21isenough <39227503+21isenough@users.noreply.github.com>
2020-04-14 11:08:31 +0200

    add comment

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = config.SECRET_KEY
 
+# Move this line out of "__main__" if run with WSGI
 db.init_app(app)
+
 api = Api(app)
 
 
