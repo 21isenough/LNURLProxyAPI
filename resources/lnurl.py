dd1540c5132feb0869aa4b3bfd6f7a9221f0e141
21isenough <39227503+21isenough@users.noreply.github.com>
2020-04-17 00:05:02 +0200

    add check for uuid in db

--- resources/lnurl.py
+++ resources/lnurl.py
@@ -33,6 +33,10 @@ class LnurlCreate(Resource):
 
 class LnurlAwait(Resource):
     def get(self, uuid):
+        db_entry = LnurlModel.find_by_uuid(uuid)
+        if not db_entry:
+            return {"status": "ERROR", "reason": "Lnurl not found."}, 404
+
         invoice = ""
         while not invoice:
             invoice = LnurlModel.find_by_uuid(uuid).invoice_bech32
